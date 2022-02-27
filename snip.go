5c30b152bf68e66d4153066cfbebc3a74f10bd48
jp <jpso.guerreiro@gmail.com>
2022-02-27 14:47:10 -0300

    .

--- snip.go
+++ snip.go
@@ -1,3 +1,5 @@
+package main
+
 import (
         "fmt"
         "log"
@@ -14,10 +16,20 @@ func main() {
         appdir := filepath.Join(usr.HomeDir, ".snipstore")
         fmt.Println(appdir)
         _, error := os.Stat(appdir)
+        fmt.Println(error)
         if error != nil && os.IsNotExist(error) {
                 errorDir := os.MkdirAll("directory", 0755)
                 if errorDir != nil {
-                        log.Fatal(error + " and " + errorDir)
+                        log.Fatal(fmt.Sprintf("%v and %s", error, errorDir))
                 }
         }
+        if len(os.Args) == 1 {
+                log.Fatal("No arguments were provided. Type 'help' to get more info.")
+                os.Exit(0)
+        }
+        var action = os.Args[1]
+        switch action {
+
+        }
+        
 }
