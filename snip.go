e3b31df0e1d17e4a9409a3d98e35dc0c013fd14d
jp <jpso.guerreiro@gmail.com>
2022-02-27 12:10:32 -0300

    go_fmt

--- snip.go
+++ snip.go
@@ -1,25 +1,23 @@
-package main
-
 import (
-    "os/user"
-    "fmt"
-	"os"
-    "log"
-	"path/filepath"
+        "fmt"
+        "log"
+        "os"
+        "os/user"
+        "path/filepath"
 )
 
 func main() {
-    	usr, err := user.Current()
-    	if err != nil {
-     	   log.Fatal( err )
-    	}
-		appdir := filepath.Join(usr.HomeDir, ".snipstore")
-		fmt.Println(appdir)
-		_, error := os.Stat(appdir)
-		if error != nil && os.IsNotExist(error) {
-		errorDir := os.MkdirAll("directory", 0755)  
- 		if errorDir != nil {  
-  			log.Fatal(error+" and "+errorDir)
-	  } 
-	}
+        usr, err := user.Current()
+        if err != nil {
+                log.Fatal(err)
+        }
+        appdir := filepath.Join(usr.HomeDir, ".snipstore")
+        fmt.Println(appdir)
+        _, error := os.Stat(appdir)
+        if error != nil && os.IsNotExist(error) {
+                errorDir := os.MkdirAll("directory", 0755)
+                if errorDir != nil {
+                        log.Fatal(error + " and " + errorDir)
+                }
+        }
 }
